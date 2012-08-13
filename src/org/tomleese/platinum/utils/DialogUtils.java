9175fb5926c1931bad3892269d6d8f949d3b6248
Tom Leese <leese.thomas81@gmail.com>
2012-08-13 19:17:31 +0100

    Added input dialogs

--- src/org/tomleese/platinum/utils/DialogUtils.java
+++ src/org/tomleese/platinum/utils/DialogUtils.java
@@ -3,6 +3,7 @@ package org.tomleese.platinum.utils;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.widget.EditText;
 
 /**
  * Provides methods to quickly and easily create dialog boxes with
@@ -10,8 +11,31 @@ import android.content.DialogInterface;
  * 
  * @author tom
  */
-public abstract class DialogUtils {
+public class DialogUtils {
 
+	/**
+	 * A callback for a text dialog.
+	 * 
+	 * @author tom
+	 *
+	 */
+	public static interface OnTextCallback {
+		/**
+		 * Called when the OK button is pressed
+		 * @param text The text of the input of the dialog
+		 * @return True if the text is valid and the dialog should be dismissed
+		 */
+		public boolean onText(String text);
+	}
+	
+	private static String getStringRes(Context context, Integer id) {
+		if (id == null) {
+			return null;
+		}
+		
+		return context.getString(id);
+	}
+	
 	/**
 	 * Shows an OK dialog box using the android OK string.
 	 * 
@@ -22,7 +46,6 @@ public abstract class DialogUtils {
 	 */
 	public static AlertDialog showOkDialog(Context context, String title, String message) {
 		AlertDialog.Builder builder = new AlertDialog.Builder(context);
-		
 		if (title != null) { builder.setTitle(title); }
 		if (message != null) { builder.setMessage(message); }
 		
@@ -51,17 +74,64 @@ public abstract class DialogUtils {
 	 * @return The new dialog box
 	 */
 	public static AlertDialog showOkDialog(Context context, Integer titleId, Integer messageId) {
-		String title = null;
-		if (titleId != null) { 
-			title = context.getString(titleId);
-		};
-		
-		String message = null;
-		if (messageId != null) {
-			message = context.getString(messageId);
-		}
+		String title = getStringRes(context, titleId);
+		String message = getStringRes(context, messageId);
 		
 		return showOkDialog(context, title, message);
 	}
 	
+	/**
+	 * Shows an OK dialog with a text input
+	 * 
+	 * @param context The context for the dialog box
+	 * @param title The title of the dialog box, can be null.
+	 * @param message The message of the dialog box, can be null.
+	 * @param text The text of the input view, can be null.
+	 * @param callback The text callback of the dialog
+	 * @return The new dialog box
+	 */
+	public static AlertDialog getString(Context context, String title, String message, String text, final OnTextCallback callback) {
+		final EditText editable = new EditText(context);
+		if (text != null) editable.setText(text);
+		
+		AlertDialog.Builder builder = new AlertDialog.Builder(context);
+		if (title != null) builder.setTitle(title);
+		if (message != null) builder.setMessage(message);
+		
+		builder.setView(editable);
+		builder.setCancelable(false);
+		
+		builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+			
+			public void onClick(DialogInterface dialog, int which) {
+				if (callback.onText(editable.getText().toString())) {
+					dialog.dismiss();
+				}
+			}
+			
+		});
+		
+		AlertDialog dialog = builder.create();
+		dialog.show();
+		
+		return dialog;
+	}
+	
+	/**
+	 * Shows an OK dialog with a text input
+	 * 
+	 * @param context The context for the dialog box
+	 * @param title The title ID of the dialog box, can be null.
+	 * @param message The message ID of the dialog box, can be null.
+	 * @param text The text ID of the input view, can be null.
+	 * @param callback The text callback of the dialog
+	 * @return The new dialog box
+	 */
+	public static AlertDialog getString(Context context, Integer titleId, Integer messageId, Integer textId, OnTextCallback callback) {
+		String title = getStringRes(context, titleId);
+		String message = getStringRes(context, messageId);
+		String text = getStringRes(context, textId);
+		
+		return getString(context, title, message, text, callback);
+	}
 }
